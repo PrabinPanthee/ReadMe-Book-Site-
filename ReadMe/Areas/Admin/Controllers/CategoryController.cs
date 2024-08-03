a095a3c868aeb09d5785f78936a816bd00e27b6f
Prabin Panthee <prabinpanthee@gmail.com>
2024-08-03 22:52:56 +0545

    Areas Defiened

--- ReadMe/Areas/Admin/Controllers/CategoryController.cs
+++ ReadMe/Areas/Admin/Controllers/CategoryController.cs
@@ -2,7 +2,7 @@
 using ReadMe.DataAccess.Repository.IRepository;
 using ReadMe.Models.Models;
 
-namespace ReadMe.Controllers
+namespace ReadMe.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
@@ -26,13 +26,14 @@ namespace ReadMe.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            if (ModelState.IsValid) {
-             _unit.categoryRepository.Add(category);
-             _unit.Save();
-             TempData["Success"] = "Category created successfully";
-             return RedirectToAction("Index");   
-            
-            
+            if (ModelState.IsValid)
+            {
+                _unit.categoryRepository.Add(category);
+                _unit.Save();
+                TempData["Success"] = "Category created successfully";
+                return RedirectToAction("Index");
+
+
             }
             return View();
 
@@ -40,37 +41,37 @@ namespace ReadMe.Controllers
         }
         [HttpGet]
         public IActionResult Edit(int? id)
-        {  
-            if(id == null || id == 0)
+        {
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            
-         var category = _unit.categoryRepository.Get(p=>p.Id == id);
-            if(category == null)
+
+            var category = _unit.categoryRepository.Get(p => p.Id == id);
+            if (category == null)
             {
                 return NotFound();
             }
-         return View(category);
+            return View(category);
         }
         [HttpPost]
-        public IActionResult Edit(Category category) 
+        public IActionResult Edit(Category category)
         {
             if (ModelState.IsValid)
-            { 
-            
-             _unit.categoryRepository.Update(category);
-             _unit.Save();
-             TempData["success"] = "Category Updated Successfully";
-             return RedirectToAction("Index");
-            
+            {
+
+                _unit.categoryRepository.Update(category);
+                _unit.Save();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+
             }
             return View();
-        
+
         }
 
         [HttpGet]
-        public IActionResult Delete(int id) 
+        public IActionResult Delete(int id)
         {
             if (id == null || id == 0)
             {
@@ -86,14 +87,14 @@ namespace ReadMe.Controllers
         [HttpPost]
         public IActionResult Delete(Category category)
         {
-                if(category is null) { return NotFound(); }
-                _unit.categoryRepository.Remove(category);
-                _unit.Save();
-                TempData["success"] = "Category Deleted Successfully";
-                return RedirectToAction("Index");
+            if (category is null) { return NotFound(); }
+            _unit.categoryRepository.Remove(category);
+            _unit.Save();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
 
-            
-            
+
+
 
 
         }
--- ReadMe/Areas/Customer/Controllers/HomeController.cs
+++ ReadMe/Areas/Customer/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ReadMe.Models;
 using System.Diagnostics;
 
-namespace ReadMe.Controllers
+namespace ReadMe.Areas.Customer.Controllers
 {
     public class HomeController : Controller
     {
